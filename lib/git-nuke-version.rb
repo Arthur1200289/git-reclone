831fc3249ce35b756b93030bc9e6cc0fa5b5f380
Jeremy Warner <jeremywrnr@gmail.com>
2017-01-31 16:58:42 -0800

    update version and readme

--- lib/git-nuke-version.rb
+++ lib/git-nuke-version.rb
@@ -1,20 +1,16 @@
 # universal version tracking
 
 class GitNuke
-  Version = "0.1.2"
+  Version = "0.2"
 end
 
 GitNuke::Help = <<-HELP
-git-nuke - git repo restoring tool.
+git-nuke - git repo restoring tool
 
-`git nuke` re-clones from the first listed remote, removing the local copy.
+re-clones from the first listed remote, overwriting the local copy.
+to restore from a particular remote repository, specify the host.
 
-to restore from a particular remote repository, specify the host:
-
-`git nuke bit`,
-`git nuke bucket`,
-`git nuke bitbucket`,
-  will all clone from a bitbucket remote.
-
+    git nuke bitbucket # reclone using bitbucket
+    git nuke github # reclone using github
 HELP
 
